c792e061e26b37d6550b5ee8605dfb015b1f6cce
leesimplon <sitrakaniaina812@gmail.com>
2019-04-01 15:02:16 +0300

    Updates

---
+++ precache-manifest.6e8261af345b23a96723d40ed58a1ce5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e470201e921e00dec1f3",
+    "url": "/sitrakAina/simplonsite.git/static/css/main.57c8f677.chunk.css"
+  },
+  {
+    "revision": "e470201e921e00dec1f3",
+    "url": "/sitrakAina/simplonsite.git/static/js/main.4e7c1f48.chunk.js"
+  },
+  {
+    "revision": "0663e274c2e39cdb7ca2",
+    "url": "/sitrakAina/simplonsite.git/static/js/runtime~main.a474a43c.js"
+  },
+  {
+    "revision": "1b5e1ed59ccbdbc5013e",
+    "url": "/sitrakAina/simplonsite.git/static/css/2.38e9618b.chunk.css"
+  },
+  {
+    "revision": "1b5e1ed59ccbdbc5013e",
+    "url": "/sitrakAina/simplonsite.git/static/js/2.7da03768.chunk.js"
+  },
+  {
+    "revision": "3d3bdcd561d63d7010e3e20238708821",
+    "url": "/sitrakAina/simplonsite.git/index.html"
+  }
+];
